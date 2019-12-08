e8a031c30989f05e4622a676354a6b451490f409
dogeofsb <43014554+dogeofsb@users.noreply.github.com>
2019-12-08 13:38:10 -0500

    Create errorListener.php

---
+++ Logging/errorListener.php
@@ -0,0 +1,11 @@
+<?php
+//php has a build in error logger, this file makes use of it
+//show errors
+ini_set('display_errors', 1);
+//log errors
+ini_set('log_errors', 1);
+//log errors to file
+ini_set('error_log', directory (file). '/log.txt');
+error_reporting(E_ALL);
+//error_log = "/tmp/php-error.log"
+?>
